81c09918143cac7c2ffe9071f4bb1ab4fa371a2b
echo <alexleel@126.com>
2024-01-31 23:59:40 +0800

    Add files via upload
    add static directory

---
+++ static/precache-manifest.7e5aaf1226900e3addfd09a42b9268f1.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd211ec696dc7b944d5",
+    "url": "css/019be8e4.d05f1162.css"
+  },
+  {
+    "revision": "cc6c23fbbee76a1e04fc",
+    "url": "css/099399dd.9310dd1b.css"
+  },
+  {
+    "revision": "b60727f85d2269d0d5c6",
+    "url": "css/0af0fca4.e3d6530d.css"
+  },
+  {
+    "revision": "1a44b4154191b0fec9de",
+    "url": "css/162d302c.9310dd1b.css"
+  },
+  {
+    "revision": "8bfe9547d910ebd66f52",
+    "url": "css/29ead7f5.9310dd1b.css"
+  },
+  {
+    "revision": "f162cc593c5b63bb8fdd",
+    "url": "css/31ad66a3.9310dd1b.css"
+  },
+  {
+    "revision": "4072a405936cc92dd33a",
+    "url": "css/524389aa.619bfb84.css"
+  },
+  {
+    "revision": "e2d093b5e17cf571781d",
+    "url": "css/61674343.9310dd1b.css"
+  },
+  {
+    "revision": "6f59972b45600a38958e",
+    "url": "css/63c47f2b.294d1efb.css"
+  },
+  {
+    "revision": "f7bb7c983377b3e660d9",
+    "url": "css/691c1182.ed0ee510.css"
+  },
+  {
+    "revision": "63a17f05089e7c0a5d2b",
+    "url": "css/7ba452e3.37efe53c.css"
+  },
+  {
+    "revision": "86ef41b5b65a42e15e04",
+    "url": "css/87fca1b4.8c8c2eec.css"
+  },
+  {
+    "revision": "ca30aeacf4b8d6e04599",
+    "url": "css/app.e4fba3f1.css"
+  },
+  {
+    "revision": "5073ed91b5c38de06d682cdee7c8adcd",
+    "url": "fonts/Eva-Icons.5073ed91.woff2"
+  },
+  {
+    "revision": "f8715d9989eadd3dfa6181d8725a4820",
+    "url": "fonts/Eva-Icons.f8715d99.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "73d4296da8b24706c689bb54b77d5b6f",
+    "url": "fonts/nunito-v11-latin-600.73d4296d.eot"
+  },
+  {
+    "revision": "b10ecee279e3a8d11d5ec3193b68d8bf",
+    "url": "fonts/nunito-v11-latin-600.b10ecee2.woff2"
+  },
+  {
+    "revision": "c3de23017bcb95715e6eb11dd6a10fc5",
+    "url": "fonts/nunito-v11-latin-600.c3de2301.woff"
+  },
+  {
+    "revision": "ed019f3de63b4d680eb428e06f6371a4",
+    "url": "fonts/nunito-v11-latin-600.ed019f3d.ttf"
+  },
+  {
+    "revision": "407e301e7f3ef97b70113a2f9330efd0",
+    "url": "fonts/nunito-v11-latin-700.407e301e.ttf"
+  },
+  {
+    "revision": "623f5ed42b0b347fb4531ded4e4b57a2",
+    "url": "fonts/nunito-v11-latin-700.623f5ed4.woff"
+  },
+  {
+    "revision": "91ae827aa880d02ea567979add1da58c",
+    "url": "fonts/nunito-v11-latin-700.91ae827a.woff2"
+  },
+  {
+    "revision": "cd3ca521e0064c0d1ecc8869acf6a459",
+    "url": "fonts/nunito-v11-latin-700.cd3ca521.eot"
+  },
+  {
+    "revision": "6386ba6af7b7a5480eb5efda82ff803c",
+    "url": "fonts/nunito-v11-latin-regular.6386ba6a.woff"
+  },
+  {
+    "revision": "a9bde0066f1398b16ba151493d70fc58",
+    "url": "fonts/nunito-v11-latin-regular.a9bde006.eot"
+  },
+  {
+    "revision": "c2f9a79dc1d2101b9beda761d1bd0494",
+    "url": "fonts/nunito-v11-latin-regular.c2f9a79d.ttf"
+  },
+  {
+    "revision": "de6068bf97f40206af0b062e262e6213",
+    "url": "fonts/nunito-v11-latin-regular.de6068bf.woff2"
+  },
+  {
+    "revision": "ba9b2e02a58599680d85a776fc1053d5",
+    "url": "img/nunito-v11-latin-600.ba9b2e02.svg"
+  },
+  {
+    "revision": "8fd515791307ea222a8c5378c2480dde",
+    "url": "img/nunito-v11-latin-700.8fd51579.svg"
+  },
+  {
+    "revision": "5106101c6cb1300b7d2837eed71677df",
+    "url": "img/nunito-v11-latin-regular.5106101c.svg"
+  },
+  {
+    "revision": "8fa1d5fb9a8f09b8cc274cfd00773bf7",
+    "url": "index.html"
+  },
+  {
+    "revision": "dfd211ec696dc7b944d5",
+    "url": "js/019be8e4.d8726e8b.js"
+  },
+  {
+    "revision": "cc6c23fbbee76a1e04fc",
+    "url": "js/099399dd.a047d401.js"
+  },
+  {
+    "revision": "b60727f85d2269d0d5c6",
+    "url": "js/0af0fca4.271bd48d.js"
+  },
+  {
+    "revision": "1a44b4154191b0fec9de",
+    "url": "js/162d302c.ce1f9159.js"
+  },
+  {
+    "revision": "8bfe9547d910ebd66f52",
+    "url": "js/29ead7f5.cd022784.js"
+  },
+  {
+    "revision": "60f592f3b636e00ba88c",
+    "url": "js/2d21e8fd.f3d2bb6c.js"
+  },
+  {
+    "revision": "f162cc593c5b63bb8fdd",
+    "url": "js/31ad66a3.12ab3f06.js"
+  },
+  {
+    "revision": "4072a405936cc92dd33a",
+    "url": "js/524389aa.21dfc9ee.js"
+  },
+  {
+    "revision": "e2d093b5e17cf571781d",
+    "url": "js/61674343.adb358dd.js"
+  },
+  {
+    "revision": "6f59972b45600a38958e",
+    "url": "js/63c47f2b.caf9b4a2.js"
+  },
+  {
+    "revision": "f7bb7c983377b3e660d9",
+    "url": "js/691c1182.5d4aa4c9.js"
+  },
+  {
+    "revision": "63a17f05089e7c0a5d2b",
+    "url": "js/7ba452e3.71a69a60.js"
+  },
+  {
+    "revision": "86ef41b5b65a42e15e04",
+    "url": "js/87fca1b4.ac9c2dc6.js"
+  },
+  {
+    "revision": "ca30aeacf4b8d6e04599",
+    "url": "js/app.841031a8.js"
+  },
+  {
+    "revision": "daead9d9873b6d3410e3",
+    "url": "js/vendor.49a1849c.js"
+  },
+  {
+    "revision": "187edcdec8d9d42c9a1f40a949473f7e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "68ca52f4af99ac8d14209829f0ea766b",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "b1dd6a46adb87d0a784d2d69490aac79",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1ebab1d3b8ea803f48edd777b2838295",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "bb91e69494132b8d1f7ab6ec3091be8a",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "29913fcd9edcf7e4da8a0e7e8b2f39c4",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "8dbc8233b6796678f5d3e89f5f5293fb",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "e2a48ccd02bdcd7ba7232d806c5b652b",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "9d593295749b7032ebd132b84061c43b",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "010d94298a0b83cc87846064dc26db44",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "68ca52f4af99ac8d14209829f0ea766b",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "ebf5d5c6805191aa981130c95ddc6240",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "c4300af6bff8c5fee475a35b169fde68",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "acffd1ad91345ae85f13a41400180311",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "6a4c3d1b28dddec6a52380503aa8d243",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "822dc3d6f00347f1a2fee56cdd615a66",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "2b566c951acd9a79b8f23613ccbcb57b",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "99f1a87494c5812192522a868f18c6b4",
+    "url": "statics/providers/consulcatalog.svg"
+  },
+  {
+    "revision": "a1f1494d7aecf90f1c5d331e9f6c71e6",
+    "url": "statics/providers/docker.svg"
+  },
+  {
+    "revision": "00a6d18e26da6c8ac8732a587aec0734",
+    "url": "statics/providers/file.svg"
+  },
+  {
+    "revision": "1a9db0d851cf1e343b2b6e386a9fae75",
+    "url": "statics/providers/internal.svg"
+  },
+  {
+    "revision": "9ea365166904899acae8a1c711a06e66",
+    "url": "statics/providers/kubernetes.svg"
+  },
+  {
+    "revision": "9ea365166904899acae8a1c711a06e66",
+    "url": "statics/providers/kubernetescrd.svg"
+  },
+  {
+    "revision": "9ea365166904899acae8a1c711a06e66",
+    "url": "statics/providers/kubernetesingress.svg"
+  },
+  {
+    "revision": "8acb71d3d65088fbd7580b6d4cdffcc5",
+    "url": "statics/providers/marathon.svg"
+  },
+  {
+    "revision": "fe355a21768e8a882234213f08a3965c",
+    "url": "statics/providers/rancher.svg"
+  },
+  {
+    "revision": "233f99392d8f73d1a2649659c80e2745",
+    "url": "statics/providers/rest.svg"
+  }
+]);
